be30baedb54a2a452248b6b9fb066d8290dc17e2
MohdSahbaz <cshabaz188@gmail.com>
2025-04-19 18:46:36 +0530

    fix about section and add new resume

--- Mohd_Sahbaz/src/pages/About.jsx
+++ Mohd_Sahbaz/src/pages/About.jsx
@@ -24,37 +24,43 @@ const About = ({ darkMode, dayMode, nightMode }) => {
           darkMode ? "bg-gray-800/[0.6] text-gray-300" : "bg-gray-100/[0.9]"
         } p-5 rounded-sm w-fit md:max-w-[720px] shadow-md mb-5`}
       >
-        <h1
-          className={`text-3xl font-bold ${
-            darkMode ? "text-white" : "text-black/[0.7]"
-          }`}
+        <div
+          className={`${
+            darkMode ? "bg-gray-800/[0.6]" : "bg-white/[0.3]"
+          } p-5 rounded-sm shadow-md`}
         >
-          About Me
-        </h1>
-        <h1
-          className={`text-2xl font-bold ${
-            darkMode ? "text-yellow-500" : "text-[#003e21]"
-          }`}
-        >
-          {aboutDetails.name}
-        </h1>
-        <p className={`mb-2 text-lg font-semibold`}>
-          {aboutDetails.shortIntro}
-        </p>
-        <p className={`mb-5 text-lg font-semibold`}>
-          {aboutDetails.descriptionOne}
-        </p>
-        <a
-          href={aboutDetails.resumeLink}
-          target="blank_"
-          className={`py-2 px-4 rounded-sm border-2 outline-none font-semibold transition-all duration-300 ${
-            darkMode
-              ? "bg-yellow-500 text-white border-cyan-800 hover:bg-yellow-800"
-              : "bg-green-300 text-black border-yellow-700 hover:bg-green-400"
-          }`}
-        >
-          Resume
-        </a>
+          <h1
+            className={`text-3xl font-bold ${
+              darkMode ? "text-white" : "text-black/[0.7]"
+            }`}
+          >
+            About Me
+          </h1>
+          <h1
+            className={`text-2xl font-bold ${
+              darkMode ? "text-yellow-500" : "text-[#003e21]"
+            }`}
+          >
+            {aboutDetails.name}
+          </h1>
+          <p className={`mb-2 text-lg font-semibold`}>
+            {aboutDetails.shortIntro}
+          </p>
+          <p className={`mb-5 text-lg font-semibold`}>
+            {aboutDetails.descriptionOne}
+          </p>
+          <a
+            href={aboutDetails.resumeLink}
+            target="blank_"
+            className={`py-2 px-4 rounded-sm border-2 outline-none font-semibold transition-all duration-300 ${
+              darkMode
+                ? "bg-yellow-500 text-white border-cyan-800 hover:bg-yellow-800"
+                : "bg-green-300 text-black border-yellow-700 hover:bg-green-400"
+            }`}
+          >
+            Resume
+          </a>
+        </div>
       </div>
     </section>
   );
